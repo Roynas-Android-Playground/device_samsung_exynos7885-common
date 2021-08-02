3acb0d876968e0decc2146fc53788942e0ad4482
LuK1337 <priv.luk@gmail.com>
2021-08-02 22:47:55 +0200

    exynos7885: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,9 @@ TARGET_KEYMASTER_VARIANT := samsung
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/device_framework_matrix.xml
 
 # NFC SKU
 ODM_MANIFEST_SKUS += NFC
