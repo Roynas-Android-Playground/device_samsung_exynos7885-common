596b4a052f975546b83f217d8c233e32b818d269
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    exynos7885-common: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -269,8 +269,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung \
-    vendor.lineage.touch@1.0-service.ss
+    vendor.lineage.touch-service.samsung
 
 # USB
 PRODUCT_PACKAGES += \
