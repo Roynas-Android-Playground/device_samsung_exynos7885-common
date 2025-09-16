359b3704df341a969f692bfacb24a21a0861e2bc
nhglong <its.nhglong@gmail.com>
2025-09-16 13:06:42 +0000

    exynos7885-common: Remove device specific FCM
    Bhoyna you're bomb P1
    
    Change-Id: I68aeeafe67d70165379fa9d7b3ae4bf06e74bb2e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,8 +74,7 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
-    $(COMMON_PATH)/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 
 # NFC SKU
 ODM_MANIFEST_SKUS += NFC
