1890bfbad73f751a89f43d47802db827e3fff677
nhglong <its.nhglong@gmail.com>
2025-09-18 17:34:18 +0000

    exynos7885-common: Decommonize AIDL touch HAL
    a30 needs ITouchscreenGesture
    
    Change-Id: I4f33e9ffe11a0016a5987f87ca8c60b6e588bf8c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,6 +70,12 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
+ifneq ($(filter a30,$(TARGET_DEVICE)),)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_touch_tsgesture.xml
+else
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_touch.xml
+endif
+
 # NFC SKU
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
