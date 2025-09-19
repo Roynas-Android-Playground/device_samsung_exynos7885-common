9f3896899d1228d971a775d6959858a33099953e
nhglong <its.nhglong@gmail.com>
2025-09-19 05:38:54 +0000

    exynos7885-common: Build 32-bit audio
    Change-Id: I60ccaceb85fda6b6d1032289f207db811798a73a

--- exynos7885-common.mk
+++ exynos7885-common.mk
@@ -19,8 +19,8 @@ AB_OTA_UPDATER := false
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
