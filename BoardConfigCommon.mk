bc9061f39f1eebab60004544afdb0d0886971673
Soo-Hwan Na <whiteshell2544@naver.com>
2025-12-20 23:43:04 +0900

    Update BoardConfigCommon.mk to remove framework matrix
    Removed device framework compatibility matrix file from the configuration.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,8 +70,7 @@ $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/e
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 
 ifneq ($(filter a30,$(TARGET_DEVICE)),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_touch_tsgesture.xml
