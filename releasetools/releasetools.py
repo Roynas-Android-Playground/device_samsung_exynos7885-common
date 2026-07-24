41f1e350ef61b2d92c54f1ab0dafbef4a8d2a0e5
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2026-07-24 08:23:13 +0000

    exynos7885-common: do not flash recovery
     * No need to traumatize the users.
    
    Change-Id: I56883b69202a86d4f2d74d249a7d82dd8f315469
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -40,5 +40,4 @@ def AddFlash(info, basename, dest):
 def OTA_InstallEnd(info):
   AddImage(info, "RADIO", "dtb.img", "/dev/block/by-name/dtb")
   AddImage(info, "IMAGES", "dtbo.img", "/dev/block/by-name/dtbo")
-  AddFlash(info, "recovery.img", "/dev/block/by-name/recovery")
   return
